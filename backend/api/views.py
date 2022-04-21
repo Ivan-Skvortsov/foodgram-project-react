c1f2293060edf57107e341c8627c722802c1d951
Ivan Skvortsov <pprofcheg@gmail.com>
2022-04-21 19:20:35 +0300

    add UserRecipeMixin

--- backend/api/views.py
+++ backend/api/views.py
@@ -39,7 +39,42 @@ class IngredientViewSet(mixins.ListModelMixin,
     pagination_class = None
 
 
-class RecipeViewSet(viewsets.ModelViewSet):
+class UserRecipeMixin:
+
+    def modify_user_to_recipe_relation(self, request, pk=None,
+                                       model_class=None):
+        """
+        Adds/removes user-recipe entry of given model_class.
+        model_class should be a child of UserRecipe model.
+        """
+        if request.method not in ['POST', 'DELETE']:
+            raise exceptions.APIException('Method not allowed')
+        user = self.request.user
+        recipe = get_object_or_404(Recipe, pk=pk)
+        related_model_entry = model_class.objects.filter(
+            user=user,
+            recipe=recipe
+        )
+        if request.method == 'POST':
+            if related_model_entry:
+                raise exceptions.ValidationError(
+                    'Error. You are already added this recipe'
+                )
+            model_class.objects.create(user=user, recipe=recipe)
+            serializer = ShortRecipeSerializer(instance=recipe)
+            return response.Response(
+                serializer.data, status=status.HTTP_201_CREATED
+            )
+        if request.method == 'DELETE':
+            if not related_model_entry:
+                raise exceptions.ValidationError(
+                    'Error. You have not added this recipe'
+                )
+            related_model_entry.delete()
+            return response.Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class RecipeViewSet(UserRecipeMixin, viewsets.ModelViewSet):
     permission_classes = (
         permissions.IsAuthenticatedOrReadOnly,
         IsAuthorOfContentOrReadOnly
@@ -67,83 +102,31 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def partial_update(self, request, *args, **kwargs):
         return super().update(request, *args, **kwargs)
 
-    @action(methods=['GET'],
-            detail=False,
+    @action(methods=['GET'], detail=False,
             permission_classes=[permissions.IsAuthenticated],
-            url_path='download_shopping_cart',
-            url_name='download_shopping_cart')
+            url_path='download_shopping_cart')
     def download_shopping_list(self, request):
         shopping_list = ShoppingListGenerator(self.request.user)
         return shopping_list.generate_pdf()
 
-    @action(methods=['POST', 'DELETE'],
-            detail=True,
+    @action(methods=['POST', 'DELETE'], detail=True,
             permission_classes=[permissions.IsAuthenticated],
-            url_path='shopping_cart',
-            url_name='shopping_cart')
+            url_path='shopping_cart')
     def modify_shopping_list(self, request, pk=None):
-        user = self.request.user
-        recipe = get_object_or_404(Recipe, pk=pk)
-        shopping_list_entry = ShoppingList.objects.filter(
-            user=user,
-            recipe=recipe
-        )
-        if request.method == 'POST':
-            if shopping_list_entry:
-                raise exceptions.ValidationError(
-                    'Error. This recipe is already in shopping list'
-                )
-            ShoppingList.objects.create(user=user, recipe=recipe)
-            serializer = ShortRecipeSerializer(instance=recipe)
-            return response.Response(
-                serializer.data, status=status.HTTP_201_CREATED
-            )
-        if request.method == 'DELETE':
-            if not shopping_list_entry:
-                raise exceptions.ValidationError(
-                    'Error. This recipe is not in your shopping list'
-                )
-            shopping_list_entry.delete()
-            return response.Response(status=status.HTTP_204_NO_CONTENT)
+        return self.modify_user_to_recipe_relation(request, pk, ShoppingList)
 
-    @action(methods=['POST', 'DELETE'],
-            detail=True,
+    @action(methods=['POST', 'DELETE'], detail=True,
             permission_classes=[permissions.IsAuthenticated],
-            url_path='favorite',
-            url_name='favorite')
+            url_path='favorite')
     def modify_favorites(self, request, pk=None):
-        user = self.request.user
-        recipe = get_object_or_404(Recipe, pk=pk)
-        favorites_entry = Favorite.objects.filter(
-            user=user,
-            recipe=recipe
-        )
-        if request.method == 'POST':
-            if favorites_entry:
-                raise exceptions.ValidationError(
-                    'Error. This recipe is already in your favorites'
-                )
-            Favorite.objects.create(user=user, recipe=recipe)
-            serializer = ShortRecipeSerializer(instance=recipe)
-            return response.Response(
-                serializer.data, status=status.HTTP_201_CREATED
-            )
-        if request.method == 'DELETE':
-            if not favorites_entry:
-                raise exceptions.ValidationError(
-                    'Error. This recipe is not in your favorites'
-                )
-            favorites_entry.delete()
-            return response.Response(status=status.HTTP_204_NO_CONTENT)
+        return self.modify_user_to_recipe_relation(request, pk, Favorite)
 
 
 class UserViewSet(DjoserUserViewSet):
 
-    @action(methods=['GET'],
-            detail=False,
+    @action(methods=['GET'], detail=False,
             permission_classes=[permissions.IsAuthenticated],
-            url_path='subscriptions',
-            url_name='subscriptions')
+            url_path='subscriptions')
     def get_subscriptions(self, request):
         users = self.request.user.subscribed_to.all()
         paginated_qs = self.paginate_queryset(users)
@@ -152,11 +135,9 @@ class UserViewSet(DjoserUserViewSet):
         serializer.context['recipes_limit'] = recipes_limit
         return self.get_paginated_response(serializer.data)
 
-    @action(methods=['POST', 'DELETE'],
-            detail=True,
+    @action(methods=['POST', 'DELETE'], detail=True,
             permission_classes=[permissions.IsAuthenticated],
-            url_path='subscribe',
-            url_name='subscribe')
+            url_path='subscribe')
     def modify_subscriptions(self, request, id=None):
         user = self.request.user
         subscription = get_object_or_404(User, id=id)
