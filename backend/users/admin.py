dff485b5d54414e253341beb0c9edd535def7acf
Ivan Skvortsov <pprofcheg@gmail.com>
2022-04-23 01:08:12 +0300

    ready for CI/CD and deploy

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 
 User = get_user_model()
@@ -9,8 +10,11 @@ admin.site.unregister(Group)
 
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    fields = ('email', 'username', 'last_name', 'first_name', 'subscribed_to',
-              'is_staff')
+class UserAdmin(BaseUserAdmin):
+    fieldsets = (
+        (None, {'fields': ('email', 'username', 'last_name', 'first_name',
+                'subscribed_to', 'password', 'is_staff')}),
+    )
     list_display = ('email', 'username', 'last_name', 'first_name')
     search_fields = ('email', 'username')
+    list_filter = ('is_staff', )
