b7a0c1906586331db4dadba2342b2fca9fe7a518
Ivan Skvortsov <pprofcheg@gmail.com>
2022-04-24 20:10:04 +0300

    corrected name of recipe validators, created new migrations

--- backend/recipes/validators.py
+++ backend/recipes/validators.py
@@ -1,7 +1,7 @@
 from django.core.exceptions import ValidationError
 
 
-def cooking_time_validator(value):
+def positive_integer_validator(value):
     if value < 1:
         raise ValidationError(
             'Значение не может быть меньше 1!'
